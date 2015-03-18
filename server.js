ab763fc2f99dced2534c3f412c288d8fc64785c0
Louise Beh <louise.beh@gmail.com>
2015-03-18 18:49:22 +0000

    Changed port to 8080

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+var port = process.env.PORT || 8080
+
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -9,8 +11,8 @@ app.get('/', function(request, response) {
   response.render('index')
 });
 
-server.listen(3000, function(){
-  console.log("server listening on port 3000");
+server.listen(8080, function(){
+  console.log("server listening on port 8080");
 });
 
 module.exports = server;
