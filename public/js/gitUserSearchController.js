1fd5e67b205305af1c67a04c9237c4c5b0fdab03
Louise Beh <louise.beh@gmail.com>
2015-03-19 11:56:56 +0000

    heroku deployment

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,12 +1,10 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var key = key;
 
   $scope.doSearch = function() {
     $scope.searchResult = searchResource.get({
       q: $scope.searchTerm,
-      access_token: key
     });
 
   };
